21fb9825c7383a24ee65fe9dbc6b97ad003ced49
remarshal <marshalwaldendev@gmail.com>
2022-03-29 14:47:10 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className='App-title'>meme generator</h2>
+        <p>React Project</p>
       </header>
     </div>
   );
