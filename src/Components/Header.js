ff1bdbc0a58447969547f25a65e77190a9abf53f
remarshal <marshalwaldendev@gmail.com>
2022-03-30 16:20:07 -0400

    added state to theme button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,29 @@
+import React, {useState} from "react"
 import logo from '../images/logo.png';
 import '../style.css';
 
 function Header() {
+    const [theme, setTheme] = useState(true)
+    
+    function chgTheme() {
+        setTheme(prevState => !prevState)
+    }
+
+    let newTheme
+    if (theme) {
+        newTheme = "Light"
+        document.querySelector("body").style.backgroundColor="black"
+    } else {
+        newTheme = "Dark"
+        document.querySelector("body").style.backgroundColor="white"
+    }
+
     return (
         <div>
             <header className="header">
                 <img src={logo} className="header-logo" alt="logo" />
                 <h2 className='header-title'>meme generator</h2>
-                <button className='header-button'>Theme Change</button>
+                <button className='header-button' onClick={chgTheme}>{newTheme} Theme</button>
             </header>
         </div>
     );
