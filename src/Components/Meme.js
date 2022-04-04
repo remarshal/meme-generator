fec3141c11dd2114a7576c0d14ae90405e5cd208
remarshal <marshalwaldendev@gmail.com>
2022-04-04 16:42:19 -0400

    state to meme component

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,16 +1,36 @@
+import React, { useState } from "react";
 import "../style.css";
-import memeData from "../data"
+import memesData from "../data";
 
 function Meme() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
+
     function newMeme() {
-        console.log("clicked")
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme(prevState => ({
+            ...prevState,
+            randomImage: url
+        }));
+        console.log(meme.randomImage);
     }
+
     return (
         <main>
             <div className="form">
                 <input type="text" className="form--input" placeholder="top text"></input>
                 <input type="text" className="form--input" placeholder="bottom text"></input>
                 <button onClick={newMeme} className="form--button">get new meme image</button>
+            </div>
+            <div>
+                <img src={meme.randomImage} className="main--img" />
             </div>
         </main>
     );
