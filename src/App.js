01371955ba332642fd545da8fe4d5846925e35d1
remarshal <marshalwaldendev@gmail.com>
2022-04-18 20:29:49 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Meme from "./Components/Meme";
+import Footer from "./Components/Footer";
 
 function App() {
    return (
       <div>
          <Header />
          <Meme />
+         <Footer />
       </div>
    );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import "../style.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h6>github.com/remarshal/meme-generator</h6>
+        </div>
+    )
+}
+
+export default Footer
